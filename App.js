6a6894fbf30593f0bed127c791cf448e8a900e40
ranjeetkumar274 <2023073045@mmmut.ac.in>
2024-11-12 11:43:48 +0530

    Updated

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 const parent = React.createElement("div", {id:"parent"},[
     React.createElement("div", {id:"child"},[
-        React.createElement("h1",{},"I'm an h1 tag!"),
+        React.createElement("h1",{},"hello there this is react from npm!"),
         React.createElement("h2",{},"I'm an h1 tag!")
      ]),
      React.createElement("div", {id:"child"},[
---
+++ README.md
@@ -0,0 +1,18 @@
+# React Again!
+
+
+# Parcel
+- Dev builds
+- Local server
+- HMR - Hot Module replacement
+- File Watching Algorithm - written in c++
+- Caching - faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- code splitting
+- differential bundeling - supports older browsers
+- diagnostic
+- error handeling
